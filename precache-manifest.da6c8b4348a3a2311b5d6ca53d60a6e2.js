3651bcf86e70b3d684dbd18464022d630f187f16
JQB <jordanqbehn@gmail.com>
2021-03-14 20:59:46 -0500

    Updates

---
+++ precache-manifest.da6c8b4348a3a2311b5d6ca53d60a6e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c65ee6b75b4170f2aef833199e77782",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "8c54ead59a2b02aa76c8",
+    "url": "/employee-directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "0199e6e660abefd1fa64",
+    "url": "/employee-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c54ead59a2b02aa76c8",
+    "url": "/employee-directory/static/js/2.d56f002a.chunk.js"
+  },
+  {
+    "revision": "5d66a7ad4de58ba65ffb",
+    "url": "/employee-directory/static/js/3.55f4b9ee.chunk.js"
+  },
+  {
+    "revision": "0199e6e660abefd1fa64",
+    "url": "/employee-directory/static/js/main.53f09368.chunk.js"
+  },
+  {
+    "revision": "53a147167e0958e92c9c",
+    "url": "/employee-directory/static/js/runtime~main.dfd0fbae.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-//Utility function for calling the random user api
-import axios from "axios"
-
-export default {
-  getRandomUsers: function() {
-
-    return axios.get("https://randomuser.me/api/?results=100")
-  }
-}
